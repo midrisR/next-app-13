17b9ea04a19fafab9afc2ad7803d80f990058189
unknown <midrisramdhani96@gmail.com>
2025-08-06 11:14:18 +0700

    change config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["localhost", "api.projectme.my.id"],
+    domains: ["projectme.my.id", "api.projectme.my.id"],
   },
 };
 
